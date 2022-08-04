d8c5ba2c00f4e09439adba1eecd15edabbec0f46
张政 <zhangzheng@sandboxol.com>
2022-08-04 20:55:00 +0800

    Chapter: Hellow Window reference: https://learnopengl-cn.github.io/01%20Getting%20started/03%20Hello%20Window/

---
+++ HelloWindow/main.cpp
@@ -0,0 +1,59 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include  <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);   //设置清空颜色缓冲时所使用的的颜色
+        glClear(GL_COLOR_BUFFER_BIT);           //清空颜色缓冲
+
+        glfwSwapBuffers(window);                //交换颜色缓冲
+        glfwPollEvents();                       //检查触发事件
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
