487808b058fa3c01d8eb08df0caeb4db360ce076
张政 <zhangzheng@sandboxol.com>
2022-09-19 11:37:48 +0800

    feat: 添加glm库

--- TemplateApp/main.cpp
+++ TemplateApp/main.cpp
@@ -2,11 +2,15 @@
 #include <Template.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 int main()
 {
+    glm::vec4 vec(1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
     system("pause");
     return 0;
 }
