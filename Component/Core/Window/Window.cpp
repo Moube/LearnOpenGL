23b241ca74a13bdd3b4db2d72b06c7fd44cef230
张政 <zhangzheng@sandboxol.com>
2022-09-20 17:23:16 +0800

    Chapter: CoordinateSystems Chapter Practice: CoordinateSystems feat: 为Window类添加新接口

--- Component/Core/Window/Window.cpp
+++ Component/Core/Window/Window.cpp
@@ -9,8 +9,10 @@ namespace Core
 		glfwInit();
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		__width = 800;
+		__height = 600;
 
-		window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+		window = glfwCreateWindow(__width, __height, "LearnOpenGL", NULL, NULL);
 		if (window == NULL)
 		{
 			std::cout << "Failed to create GLFW window" << std::endl;
@@ -42,19 +44,34 @@ namespace Core
 			glfwSetWindowShouldClose(window, true);
 	}
 
+	void Window::updateSize(int width, int height)
+	{
+		__width = width;
+		__height = height;
+	}
+
+	void Window::getSize(int& width, int& height)
+	{
+		width = __width;
+		height = __height;
+	}
+
+	void Window::PreProcess()
+	{
+		processInput();
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Çå³ýÉî¶È»º´æ
+	}
+
+	void Window::PostProcess()
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
 	void Window::FramebufferSizeCallback(GLFWwindow* window, int width, int height)
 	{
 		glViewport(0, 0, width, height);
+		Window::Instance()->updateSize(width, height);
 	}
-
-	Window::Window()
-	{
-
-	}
-
-	Window::~Window()
-	{
-
-	}
-
 }
--- Component/Core/Window/Window.h
+++ Component/Core/Window/Window.h
@@ -12,17 +12,29 @@ namespace Core
         bool Init();
 
         GLFWwindow* GetWindow();
-
         GLFWwindow* window;
-
         void processInput();
+
+        void updateSize(int width, int height);
+        void getSize(int& width, int& height);
+        int getWidth() { return __width; };
+        int getHeight() { return __height; };
+
+        void PreProcess();
+        void PostProcess();
 
     public:
         static void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+        int __width;
+        int __height;
 
     private:
-        Window();
-        ~Window();
+        Window() {};
+        ~Window() {};
+        Window(const Window&) = delete;
+        Window& operator=(const Window&) = delete;
+        Window(Window&& other) noexcept = default;
+        Window& operator=(Window&& other) noexcept = default;
         friend class Singleton<Window>;
     };
 }
