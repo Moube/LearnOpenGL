7612e02ccc4e6800f26e8b1b0595387c705c8d0a
Moube <429687935@qq.com>
2022-09-28 15:02:59 +0800

    feat: 添加assimp库

--- TemplateApp/main.cpp
+++ TemplateApp/main.cpp
@@ -6,11 +6,13 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <assimp/color4.h>
 
 int main()
 {
     glm::vec4 vec(1.0f);
     glm::mat4 trans = glm::mat4(1.0f);
+    aiColor4t<float> assimpColor4(1.0f, 1.0f, 1.0f, 1.0f);
     system("pause");
     return 0;
 }
